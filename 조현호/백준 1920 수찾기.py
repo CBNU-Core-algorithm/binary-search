a602caad36ba3af2c7d2f81491639182d961c206
aesthetics-of-record <111675679+aesthetics-of-record@users.noreply.github.com>
2023-01-08 23:12:01 +0900

    Update 백준 1920 수찾기.py

--- 조현호/백준 1920 수찾기.py
+++ 조현호/백준 1920 수찾기.py
@@ -1,9 +1,11 @@
 import sys
+# sys.stdin = open('a.txt', 'rt') # test용 코드
 input = sys.stdin.readline
 
-input()
-data = set(input().split())
-input()
-check_data = list(input().split())
+# 값 입력받기
+n = int(input())
+n_list = set(map(int, input().strip().split())) # set을 이용해서 in 시간복잡도 O(1)으로 줄이기
+m = int(input())
+m_list = list(map(int, input().strip().split()))
 
-print('\n'.join(list(map(lambda x: '1' if x in data else '0', [x for x in check_data]))))
+print('\n'.join(map(lambda x : '1' if x in n_list else '0', m_list)))
